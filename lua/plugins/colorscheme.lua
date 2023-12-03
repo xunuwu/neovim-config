5353bed29e6ac766f220c60df53f006f31ec8a72
xunuwu <xunuwu@gmail.com>
2023-12-04 00:18:44 +0100

    change colorscheme to github_dark_default

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,7 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
+	"projekt0n/github-nvim-theme",
 	priority = 1000,
 	config = function()
-		vim.cmd([[colorscheme rose-pine]])
+		vim.cmd([[colorscheme github_dark_default]])
 	end,
 }
