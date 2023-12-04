9eecd0dae9f2cc95e38de73dbffe72820ca181f0
xunuwu <xunuwu@gmail.com>
2023-12-04 22:08:38 +0100

    change statusline to heirline (incomplete)

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,5 +1,296 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {},
+	"rebelot/heirline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons", "lewis6991/gitsigns.nvim" },
+	opts = function()
+		local conditions = require("heirline.conditions")
+		local utils = require("heirline.utils")
+
+		local colors = require("github-theme.palette").load("github_dark_default")
+
+		require("heirline").load_colors({
+			black = colors.black.base,
+			red = colors.red.base,
+			green = colors.green.base,
+			yellow = colors.yellow.base,
+			blue = colors.blue.base,
+			purple = colors.magenta.base,
+			cyan = colors.cyan.base,
+			white = colors.white.base,
+			orange = colors.orange,
+			pink = colors.pink.base,
+		})
+
+		local ViMode = {
+			init = function(self)
+				self.mode = vim.fn.mode(1)
+			end,
+			static = {
+				mode_names = {
+					n = "N",
+					no = "N?",
+					nov = "N?",
+					noV = "N?",
+					["no\22"] = "N?",
+					niI = "Ni",
+					niR = "Nr",
+					niV = "Nv",
+					nt = "Nt",
+					v = "V",
+					vs = "Vs",
+					V = "V_",
+					Vs = "Vs",
+					["\22"] = "^V",
+					["\22s"] = "^V",
+					s = "S",
+					S = "S_",
+					["\19"] = "^S",
+					i = "I",
+					ic = "Ic",
+					ix = "Ix",
+					R = "R",
+					Rc = "Rc",
+					Rx = "Rx",
+					Rv = "Rv",
+					Rvc = "Rv",
+					Rvx = "Rv",
+					c = "C",
+					cv = "Ex",
+					r = "...",
+					rm = "M",
+					["r?"] = "?",
+					["!"] = "!",
+					t = "T",
+				},
+				mode_colors = {
+					n = "red",
+					i = "green",
+					v = "cyan",
+					V = "cyan",
+					["\22"] = "cyan",
+					c = "orange",
+					s = "purple",
+					S = "purple",
+					["\19"] = "purple",
+					R = "orange",
+					["!"] = "red",
+					t = "red",
+				},
+			},
+			provider = function(self)
+				return " %2(" .. self.mode_names[self.mode] .. "%)"
+			end,
+			hl = function(self)
+				local mode = self.mode:sub(1, 1) -- get only the first mode character
+				return { fg = self.mode_colors[mode], bold = true }
+			end,
+			-- Re-evaluate the component only on ModeChanged event!
+			-- Also allows the statusline to be re-evaluated when entering operator-pending mode
+			update = {
+				"ModeChanged",
+				pattern = "*:*",
+				callback = vim.schedule_wrap(function()
+					vim.cmd("redrawstatus")
+				end),
+			},
+		}
+
+		local LSPActive = {
+			condition = conditions.lsp_attached,
+			update = { "LspAttach", "LspDetach", "BufEnter" },
+			on_click = {
+				callback = function()
+					vim.defer_fn(function()
+						vim.cmd("LspInfo")
+					end, 100)
+				end,
+				name = "heirline_LSP",
+			},
+			provider = function()
+				local names = {}
+				for i, server in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+					table.insert(names, server.name)
+				end
+				return " " .. table.concat(names, " ")
+			end,
+			hl = { fg = "gray", bold = true },
+		}
+
+		local FileNameBlock = {
+			-- let's first set up some attributes needed by this component and it's children
+			init = function(self)
+				self.filename = vim.api.nvim_buf_get_name(0)
+			end,
+		}
+		-- We can now define some children separately and add them later
+
+		local FileIcon = {
+			init = function(self)
+				local filename = self.filename
+				local extension = vim.fn.fnamemodify(filename, ":e")
+				self.icon, self.icon_color =
+					require("nvim-web-devicons").get_icon_color(filename, extension, { default = true })
+			end,
+			provider = function(self)
+				return self.icon and (self.icon .. " ")
+			end,
+			hl = function(self)
+				return { fg = self.icon_color }
+			end,
+		}
+
+		local FileName = {
+			provider = function(self)
+				-- first, trim the pattern relative to the current directory. For other
+				-- options, see :h filename-modifers
+				local filename = vim.fn.fnamemodify(self.filename, ":.")
+				if filename == "" then
+					return "[No Name]"
+				end
+				-- now, if the filename would occupy more than 1/4th of the available
+				-- space, we trim the file path to its initials
+				-- See Flexible Components section below for dynamic truncation
+				if not conditions.width_percent_below(#filename, 0.25) then
+					filename = vim.fn.pathshorten(filename)
+				end
+				return filename
+			end,
+			hl = { fg = utils.get_highlight("Directory").fg },
+		}
+
+		local FileFlags = {
+			{
+				condition = function()
+					return vim.bo.modified
+				end,
+				provider = "[+]",
+				hl = { fg = "green" },
+			},
+			{
+				condition = function()
+					return not vim.bo.modifiable or vim.bo.readonly
+				end,
+				provider = "",
+				hl = { fg = "orange" },
+			},
+		}
+
+		-- Now, let's say that we want the filename color to change if the buffer is
+		-- modified. Of course, we could do that directly using the FileName.hl field,
+		-- but we'll see how easy it is to alter existing components using a "modifier"
+		-- component
+
+		local FileNameModifer = {
+			hl = function()
+				if vim.bo.modified then
+					-- use `force` because we need to override the child's hl foreground
+					return { fg = "cyan", bold = true, force = true }
+				end
+			end,
+		}
+
+		-- let's add the children to our FileNameBlock component
+		FileNameBlock = utils.insert(
+			FileNameBlock,
+			FileIcon,
+			utils.insert(FileNameModifer, FileName), -- a new table where FileName is a child of FileNameModifier
+			FileFlags,
+			{ provider = "%<" } -- this means that the statusline is cut here when there's not enough space
+		)
+
+		local WorkDir = {
+			provider = function()
+				local icon = " "
+				local cwd = vim.fn.getcwd(0)
+				cwd = vim.fn.fnamemodify(cwd, ":~")
+				if not conditions.width_percent_below(#cwd, 0.25) then
+					cwd = vim.fn.pathshorten(cwd)
+				end
+				local trail = cwd:sub(-1) == "/" and "" or "/"
+				return icon .. cwd .. trail
+			end,
+			hl = { fg = "blue", bold = true },
+		}
+
+		local Git = {
+			condition = conditions.is_git_repo,
+
+			init = function(self)
+				self.status_dict = vim.b.gitsigns_status_dict
+				self.has_changes = self.status_dict.added ~= 0
+					or self.status_dict.removed ~= 0
+					or self.status_dict.changed ~= 0
+			end,
+
+			hl = { fg = "pink" },
+
+			{ -- git branch name
+				provider = function(self)
+					return " " .. self.status_dict.head
+				end,
+				hl = { bold = true },
+			},
+			-- You could handle delimiters, icons and counts similar to Diagnostics
+			{
+				condition = function(self)
+					return self.has_changes
+				end,
+				provider = "(",
+			},
+			{
+				provider = function(self)
+					local count = self.status_dict.added or 0
+					return count > 0 and ("+" .. count)
+				end,
+				hl = { fg = "green" },
+			},
+			{
+				provider = function(self)
+					local count = self.status_dict.removed or 0
+					return count > 0 and ("-" .. count)
+				end,
+				hl = { fg = "red" },
+			},
+			{
+				provider = function(self)
+					local count = self.status_dict.changed or 0
+					return count > 0 and ("~" .. count)
+				end,
+				hl = { fg = "orange" },
+			},
+			{
+				condition = function(self)
+					return self.has_changes
+				end,
+				provider = ")",
+			},
+		}
+
+		local Align = { provider = "%=" }
+		local Space = { provider = " " }
+
+      -- stylua: ignore
+      local DefaultStatusLine = {
+         Space, ViMode, Space, WorkDir, Space, FileNameBlock, Align,
+         Align,
+         LSPActive, Space, Git, Space
+      }
+
+		local StatusLines = {
+			hl = function()
+				if conditions.is_active() then
+					return "StatusLine"
+				else
+					return "StatusLineNC"
+				end
+			end,
+
+			fallthrough = false,
+
+			DefaultStatusLine,
+		}
+
+		return {
+			statusline = StatusLines,
+		}
+	end,
 }
