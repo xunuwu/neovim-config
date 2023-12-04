c1e088d939062a4d3f7dec109d24e3e8b045263d
xunuwu <xunuwu@gmail.com>
2023-12-04 15:03:55 +0100

    override cursorline color fr this time

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,13 +2,7 @@ return {
 	"projekt0n/github-nvim-theme",
 	priority = 1000,
 	config = function()
-		require("github-theme").setup({
-			groups = {
-				github_dark_default = {
-					CursorLine = { bg = "#202020" },
-				},
-			},
-		})
 		vim.cmd([[colorscheme github_dark_default]])
+		vim.cmd([[highlight CursorLine guibg=#202020]])
 	end,
 }
