55edf99ea585b6f224974df66b76b8ec50805f08
xunuwu <xunuwu@gmail.com>
2023-12-05 21:52:24 +0100

    small keymap change

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,6 +5,6 @@ vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { desc = "Rename" })
 vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, { desc = "Code Action" })
 vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, { desc = "Definition" })
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.references, { desc = "References" })
-vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, { desc = "Info" })
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Info" })
 
 vim.keymap.set("n", "<leader>ol", "<cmd>Lazy<cr>", { desc = "Lazy" })
