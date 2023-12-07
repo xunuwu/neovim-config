c694dbf66855593243d5d5466a2f3ea091631995
xunuwu <xunuwu@gmail.com>
2023-12-07 02:27:51 +0100

    add neotree

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,19 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	keys = {
+		{ "<leader>o;", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+	},
+	opts = {
+		close_if_last_window = true,
+		filesystem = {
+			follow_current_file = { enabled = true },
+			use_libuv_file_watcher = true,
+		},
+	},
+}
