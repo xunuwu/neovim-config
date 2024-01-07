bfa0fd78c71afcf2d7278dbd5d5c36e9e9aff116
xunuwu <xunuwu@gmail.com>
2024-01-07 13:17:11 +0100

    oxocarbon fix

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,6 @@
 return {
 	"nyoom-engineering/oxocarbon.nvim",
+	commit = "b52c62f688865ef1e1184c1a9fdcf82ca9f9d4c5", -- TODO remove when it gets merged
 	priority = 1000,
 	config = function()
 		vim.opt.background = "dark"
