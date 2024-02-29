d463952192cf2439eb134dd31de0656817a806e8
xunuwu <xunuwu@gmail.com>
2024-02-29 19:53:21 +0100

    tadd toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"akansho/toggleterm.nvim",
+		version = "v2.10.0",
+		--keys = {
+		--{ [[<c-\>]], "<cmd>ToggleTerm<cr>", mode = { "n", "i", "t" }, desc = "Toggle ToggleTerm" },
+		--},
+		opts = {
+			open_mapping = [[<c-\>]],
+			autochdir = true,
+		},
+	},
+}
