ee69aee1c91ffc3804a60a5659ae0078ac4d3512
xunuwu <xunuwu@gmail.com>
2024-02-29 19:53:51 +0100

    toggleterm remove unused lazy.nvim keys section

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,9 +2,6 @@ return {
 	{
 		"akansho/toggleterm.nvim",
 		version = "v2.10.0",
-		--keys = {
-		--{ [[<c-\>]], "<cmd>ToggleTerm<cr>", mode = { "n", "i", "t" }, desc = "Toggle ToggleTerm" },
-		--},
 		opts = {
 			open_mapping = [[<c-\>]],
 			autochdir = true,
