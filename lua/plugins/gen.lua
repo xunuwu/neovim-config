fc8d89b9e3f794e4fadcf3e50371b4cc3e443ac9
xunuwu <xunuwu@gmail.com>
2024-04-02 22:23:32 +0200

    gen.nvim with gemma:7b

---
+++ lua/plugins/gen.lua
@@ -0,0 +1,12 @@
+return {
+	"David-Kunz/gen.nvim",
+	keys = {
+		{ "<leader>g", "<cmd>Gen<CR>", desc = "gen.nvim" },
+	},
+	opts = {
+		model = "gemma:7b",
+		host = "localhost",
+		port = "11434",
+		show_mode = true,
+	},
+}
